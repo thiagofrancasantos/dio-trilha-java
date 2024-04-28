b3e749cca6039e8afb428a3c5cc69e8ae96a858d
Thiago França <francasantos.t@gmail.com>
2024-04-28 15:50:45 -0400

    tipos e casos de uso de comentarios e como gerar documentaçaõ d aplicação

---
+++ terminal/src/Calculadora.java
@@ -0,0 +1,25 @@
+
+// COMANDO PARA GERAR DOCUMENTAÇÃO DO PROGRAMA JAVA
+// javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java 
+
+
+/**
+ * @author Thiago França  
+   @version 1.0
+   @since 28/04/2024 
+ */
+
+public class Calculadora {
+
+    /**
+     * Este método é utilizado para somar dois números inteiros
+       @param numeroUM este é o primeiro parâmetro do método
+       @param numeroDois este é o segundo parâmetro do método
+       @return int o resultado deste método é a soma de dois números inteiros
+     */
+    
+
+     public int somar (int numeroUM, int numeroDois){
+        return numeroUM + numeroDois;
+     }
+}
