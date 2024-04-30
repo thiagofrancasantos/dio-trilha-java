b30ce3303dabe0a9c01499d8013ee0053dd2dca7
Thiago França <francasantos.t@gmail.com>
2024-04-29 20:24:50 -0400

    uso de estruturas condicionais, de repetição e excepcionais

---
+++ controle-de-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,14 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.00;
+        double valorSolicitado = 17.0;
+        
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado; 
+            System.out.println("Saldo atualizado: " + saldo);
+
+        }else
+            System.out.println("Saldo insuficiente!");
+        
+    }
+}
---
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,19 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if(nota >= 7)
+            System.out.println("APROVADO!");
+
+        else if(nota >= 5 && nota <7)
+            System.out.println("RECUPERAÇÃO!");
+
+        else
+            System.out.println("REPROVADO!");
+
+
+        //Condição ternária
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
